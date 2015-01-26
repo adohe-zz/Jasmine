573d0f4275183a04325bd8efa0a7e78ae9e82a11
TonyAdo <coolhzb@163.com>
2015-01-26 16:08:33 +0800

    finish the GateFilter interface...

--- jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/AbstractGateFilter.java
+++ jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/AbstractGateFilter.java
@@ -3,6 +3,20 @@ package com.xqbase.gatekeeper.tcp.gate;
 /**
  * Base abstract class for GateFilters. This base class
  * defines other abstract class.
+ *
+ * @author Tony He
  */
-public abstract class AbstractGateFilter implements GateFilter {
+public abstract class AbstractGateFilter implements GateFilter, Comparable<AbstractGateFilter> {
+
+    /**
+     * By default GateFilters are static, they don't carry state.
+     */
+    public boolean isStaticFilter() {
+        return true;
+    }
+
+    @Override
+    public int compareTo(AbstractGateFilter filter) {
+        return this.filterOrder() - filter.filterOrder();
+    }
 }
--- jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/GateFilter.java
+++ jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/GateFilter.java
@@ -8,6 +8,21 @@ package com.xqbase.gatekeeper.tcp.gate;
 public interface GateFilter {
 
     /**
+     * To classify a filter by type. Standard types in GateKeeper are "pre"
+     * for pre-routing filtering. "route" for routing to origin. "post" for
+     * post-routing filtering. "error" for error handling. We also support
+     * "static" filter for static response filtering.
+     */
+    String filterType();
+
+    /**
+     * filterOrder() must also be defined for a filter. Filters may have the
+     * same filter order if precedence is not important for a filter. filterOrder
+     * do not need to be sequential.
+     */
+    int filterOrder();
+
+    /**
      * a "true" return from this method means that the run() method should
      * be invoked.
      */
