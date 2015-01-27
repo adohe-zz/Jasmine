422a1e128fb50e867b5794b589fe220a53d38350
TonyAdo <coolhzb@163.com>
2015-01-27 11:12:21 +0800

    finish the FilterFileManager...

--- jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/FilterFileManager.java
+++ jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/FilterFileManager.java
@@ -3,6 +3,12 @@ package com.xqbase.gatekeeper.tcp.gate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * This class manages the directory polling for changes
  * and new filters.
@@ -17,6 +23,7 @@ public class FilterFileManager {
     private int pollingInterval;
 
     private Thread poller;
+    private boolean running = true;
 
     private static FilterFileManager INSTANCE;
 
@@ -36,7 +43,80 @@ public class FilterFileManager {
 
         INSTANCE.pollingInterval = pollingInterval;
         INSTANCE.aDirectories = directories;
+        INSTANCE.manageFiles();
+        INSTANCE.startPoller();
     }
 
+    public static void shutdown() {
+        INSTANCE.stopPoller();
+    }
 
+    /**
+     * Returns a list of all files from polled directory.
+     */
+    private List<File> getFiles() {
+        List<File> files = new ArrayList<File>();
+        for (String dict : aDirectories) {
+            if (dict != null) {
+                File directory = getDirectory(dict);
+                File[] aFiles = directory.listFiles();
+                if (aFiles != null) {
+                    files.addAll(Arrays.asList(aFiles));
+                }
+            }
+        }
+
+        return files;
+    }
+
+    /**
+     * Returns the directory file for the given path. A Runtime Exception
+     * will throw if the directory is not invalid.
+     */
+    private File getDirectory(String path) {
+        File dict = new File(path);
+        if (!dict.isDirectory()) {
+            URL resource = FilterFileManager.class.getClassLoader().getResource(path);
+            try {
+                dict = new File(resource.toURI());
+            } catch (Exception e) {
+                LOGGER.error("Error accessing directory in class loader. path=" + path, e);
+            }
+            if (!dict.isDirectory()) {
+                throw new RuntimeException(dict.getAbsolutePath() + " is not a valid directory");
+            }
+        }
+        return dict;
+    }
+
+    /**
+     * Start the polling for filters.
+     */
+    private void startPoller() {
+        poller = new Thread("FilterFileManagerPoller") {
+            @Override
+            public void run() {
+                while (running) {
+                    try {
+                        sleep(pollingInterval * 1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        poller.start();
+    }
+
+    private void stopPoller() {
+        running = false;
+    }
+
+    private void manageFiles() {
+        List<File> files = getFiles();
+        processFilterFile(files);
+    }
+
+    private void processFilterFile(List<File> files) {
+    }
 }
