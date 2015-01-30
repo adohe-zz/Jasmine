4532b62cb5bc6bd11d226f6cb6947c4dbb4b1067
TonyAdo <coolhzb@163.com>
2015-01-30 14:36:00 +0800

    add the GroovyCompiler...

--- jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/groovy/GroovyCompiler.java
+++ jasmine-gatekeeper-server/src/main/java/com/xqbase/gatekeeper/tcp/gate/groovy/GroovyCompiler.java
@@ -1,6 +1,9 @@
 package com.xqbase.gatekeeper.tcp.gate.groovy;
 
 import com.xqbase.gatekeeper.tcp.gate.DynamicCodeCompiler;
+import groovy.lang.GroovyClassLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
@@ -11,13 +14,25 @@ import java.io.File;
  */
 public class GroovyCompiler implements DynamicCodeCompiler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GroovyCompiler.class);
+
     @Override
     public Class compile(String code, String name) throws Exception {
-        return null;
+        GroovyClassLoader classLoader = getClassLoader();
+        LOGGER.warn("Compiling filter: " + name);
+        Class groovyClass = classLoader.parseClass(code, name);
+        return groovyClass;
     }
 
     @Override
     public Class compile(File file) throws Exception {
-        return null;
+        GroovyClassLoader classLoader = getClassLoader();
+        LOGGER.warn("Compiling filter: " + file.getName());
+        Class groovyClass = classLoader.parseClass(file);
+        return groovyClass;
+    }
+
+    private GroovyClassLoader getClassLoader() {
+        return new GroovyClassLoader();
     }
 }
